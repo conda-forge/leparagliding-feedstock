ae4048aed3c3c8e5297ff3f2f6136577dbfe9f72
looooo <sppedflyer@gmail.com>
2025-01-06 00:51:09 +0100

    try another attempt for osx-arm

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,8 @@
-${FC} ${FFLAGS} leparagliding.f -L${PREFIX}/lib -o leparagliding
+if [[ ${target_platform} =~ osx-64 ]]; then
+    export LIBRARY_PATH=${PREFIX}/lib:$LIBRARY_PATH
+    export DYLD_LIBRARY_PATH=${PREFIX}/lib:$DYLD_LIBRARY_PATH
+fi
+
+${FC} ${FFLAGS} leparagliding.f -o leparagliding
 mkdir -p ${PREFIX}/bin
 cp leparagliding ${PREFIX}/bin/

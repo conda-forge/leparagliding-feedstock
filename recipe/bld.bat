4fe3c377d7ec04200a496c4b64167c00e0c20a2f
looooo <sppedflyer@gmail.com>
2025-01-05 17:04:40 +0100

    win: try another attempt

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,8 @@
-%FC% -std=legacy leparagliding.f
+if defined FFLAGS (
+    set FFLAGS=%FFLAGS% -std=legacy
+) else (
+    set FFLAGS=-std=legacy
+)
+%FC% leparagliding.f
 mkdir %LIBRARY_PREFIX%\bin
 COPY a.exe %LIBRARY_PREFIX%\bin\leparagliding.exe

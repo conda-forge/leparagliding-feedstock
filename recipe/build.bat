4bdd1901b29c1aa01ec31e77d9a31d5d833e91d2
looooo <sppedflyer@gmail.com>
2025-01-05 17:50:52 +0100

    -std=f2018

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,7 +1,7 @@
 if defined FFLAGS (
-    set FFLAGS=%FFLAGS% -std=legacy
+    set FFLAGS=%FFLAGS% -std=f2018
 ) else (
-    set FFLAGS=-std=legacy
+    set FFLAGS=-std=f2018
 )
 %FC% %FFLAGS% leparagliding.f
 mkdir %LIBRARY_PREFIX%\bin

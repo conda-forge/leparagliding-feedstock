a2fe48591d47afe0cdc1a57ed02870bd6dea4c45
looooo <sppedflyer@gmail.com>
2025-01-05 17:00:22 +0100

    win: simplify build-script

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,3 @@
-if defined FFLAGS (
-    set FFLAGS=%FFLAGS% -std=legacy
-) else (
-    set FFLAGS=-std=legacy
-)
-%FC% %FFLAGS% leparagliding.f
+%FC% -std=legacy leparagliding.f
 mkdir %LIBRARY_PREFIX%\bin
 COPY a.exe %LIBRARY_PREFIX%\bin\leparagliding.exe
